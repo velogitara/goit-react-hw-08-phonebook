41b44e9cb963ed4bcf47c4caf6cc756c3a9b40b1
Oleksandr Pavlishchev <velogitara@gmail.com>
2022-09-04 22:26:22 +0300

    add notify for contacts

--- src/components/Routes/PrivateRoute.jsx
+++ src/components/Routes/PrivateRoute.jsx
@@ -1,8 +1,15 @@
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import authSelectors from 'redux/auth-selectors';
+import { Notify } from 'notiflix';
 
 export default function PrivateRoute({ children }) {
   const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+
+  if (!isLoggedIn) {
+    Notify.failure('Please login first', {
+      position: 'center-center',
+    });
+  }
   return isLoggedIn ? children : <Navigate to="/login" replace />;
 }
