79a850e55e14ecfd594a0640452eb60d45693960
Oleksandr Pavlishchev <93466688+velogitara@users.noreply.github.com>
2022-09-08 21:52:02 +0300

    12

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,13 +1,13 @@
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector} from 'react-redux';
 import authSelectors from 'redux/auth-selectors';
-import authOperations from 'redux/auth-operations';
+// import authOperations from 'redux/auth-operations';
 import defaultAvatar from './default-avatar.png';
 import { Div } from './UserMenu.styled';
 import {useNavigate} from 'react-router-dom'
 
 export default function UserMenu() {
   const email = useSelector(authSelectors.getUserEmail);
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
   const navigate = useNavigate();
   const logOutHandler = () => {
     navigate("/login", {replace: true})
