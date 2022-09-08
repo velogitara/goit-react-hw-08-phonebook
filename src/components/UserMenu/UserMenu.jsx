15a238b59a055569dcd2e2c6c73f9a095cc57240
Oleksandr Pavlishchev <93466688+velogitara@users.noreply.github.com>
2022-09-08 21:40:51 +0300

    add navigate to logout btn

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -3,15 +3,22 @@ import authSelectors from 'redux/auth-selectors';
 import authOperations from 'redux/auth-operations';
 import defaultAvatar from './default-avatar.png';
 import { Div } from './UserMenu.styled';
+import {useNavigate} from 'react-router-dom'
 
 export default function UserMenu() {
   const email = useSelector(authSelectors.getUserEmail);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const logOutHandler = () => {
+    navigate("/login", {replace: true})
+    dispatch(authOperations.logOut())
+
+  }
   return (
     <Div>
       <img src={defaultAvatar} alt="" width="32" />
       <span>Ласкаво Просимо, {email} </span>
-      <button type="button" onClick={() => dispatch(authOperations.logOut())}>
+      <button type="button" onClick={}>
         Вихід
       </button>
     </Div>
